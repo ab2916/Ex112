9ca3c27158700f3604de54323dc75e1b6628d291
ab2916-Erel <erel10bb@gmail.com>
2023-05-11 11:19:04 +0300

    initial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,16 +8,43 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * The type Main activity.
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * The Count.
+     */
     int count = 0;
 
+    /**
+     * The Et.
+     */
     EditText et;
+    /**
+     * The Count key.
+     */
     static final String COUNT_KEY = "count";
+    /**
+     * The String key.
+     */
     static final String STRING_KEY = "string";
 
+    /**
+     * The Settings.
+     */
     SharedPreferences settings;
+    /**
+     * The Output text.
+     */
     TextView outputText;
+    /**
+     * The Str.
+     */
     String str;
+    /**
+     * The Num.
+     */
     int num;
 
     @Override
@@ -34,16 +61,32 @@ public class MainActivity extends AppCompatActivity {
         str = settings.getString(STRING_KEY,"");
         et.setText(str);
     }
+
+    /**
+     * Count.
+     *
+     * @param view the view
+     */
     public void count(View view) {
         count++;
         outputText.setText(Integer.toString(count));
     }
 
+    /**
+     * Reset.
+     *
+     * @param view the view
+     */
     public void reset(View view) {
         count = 0;
         outputText.setText(Integer.toString(count));
     }
 
+    /**
+     * Exit.
+     *
+     * @param view the view
+     */
     public void exit(View view) {
         str = et.getText().toString();
         //settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
